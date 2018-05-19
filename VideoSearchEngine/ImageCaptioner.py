4939c2cc8d22e0453d2591e77246200c02c7b408
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2018-05-18 22:29:35 -0700

    Initial Image Captioner Files

---
+++ VideoSearchEngine/ImageCaptioner.py
@@ -0,0 +1,15 @@
+# Main file for generating text for an image
+
+# Main API is here, more files may be used for the implementation
+
+def get_image_captioner():
+    '''
+    return the version specified in the configuration to use
+    e.g. if there is a basic one and a complex one, the configuration should be able
+    to decide which one to use
+    '''
+    return None
+
+'''
+Describe API supported here
+'''
