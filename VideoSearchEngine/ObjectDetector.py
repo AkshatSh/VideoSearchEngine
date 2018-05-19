061982fca7f798c226a61d4498f07ed6276eb102
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2018-05-18 22:29:49 -0700

    Initial Object Detector Files

---
+++ VideoSearchEngine/ObjectDetector.py
@@ -0,0 +1,15 @@
+# Main file for the bounding box object detection problem
+
+# Main API is here, more files may be used for the implementation
+
+def get_object_detector():
+    '''
+    return the version specified in the configuration to use
+    e.g. if there is a basic one and a complex one, the configuration should be able
+    to decide which one to use
+    '''
+    return None
+
+'''
+Describe API supported here
+'''
