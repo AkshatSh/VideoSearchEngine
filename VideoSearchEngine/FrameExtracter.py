afe078a8f03ab2844f56e8c353228fdfb5bb49c3
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2018-05-18 22:29:26 -0700

    Initial Frame Extracter Files

---
+++ VideoSearchEngine/FrameExtracter.py
@@ -0,0 +1,16 @@
+# Main file for extracting the useful and non redundant subsequnce of
+# frames from a series of frames
+
+# Main API is here, more files may be used for the implementation
+
+def get_fram_extracter():
+    '''
+    return the version specified in the configuration to use
+    e.g. if there is a basic one and a complex one, the configuration should be able
+    to decide which one to use
+    '''
+    return None
+
+'''
+Describe API supported here
+'''
