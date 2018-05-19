f99143c291cb0fd352c7d0a852c152700e34ea6e
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2018-05-18 22:09:29 -0700

    added initial database utils

---
+++ VideoSearchEngine/database_utils.py
@@ -0,0 +1,28 @@
+'''
+This util package contains all the information to interact with the database storing the videos
+'''
+
+
+def get_entry(id):
+    pass
+
+def get_summary(id):
+    pass
+
+def get_video_name(id):
+    pass
+
+def get_id_from_name(name):
+    pass
+
+def get_all_ids():
+    pass
+
+def get_all_data():
+    pass
+
+def upload_new_summary(video_name, video_summary):
+    pass
+
+def update_summary(video_name, video_summary):
+    pass
