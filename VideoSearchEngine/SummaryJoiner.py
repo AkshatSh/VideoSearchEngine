e409d57aee6b67ccb75d84ad32748fbc4f9d8482
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2018-05-18 22:29:56 -0700

    Initial Summary Joiner files

---
+++ VideoSearchEngine/SummaryJoiner.py
@@ -0,0 +1,16 @@
+# Main file for given a set of summaries join them to create a large coherent
+# summary
+
+# Main API is here, more files may be used for the implementation
+
+def get_video_distributor():
+    '''
+    return the version specified in the configuration to use
+    e.g. if there is a basic one and a complex one, the configuration should be able
+    to decide which one to use
+    '''
+    return None
+
+'''
+Describe API supported here
+'''
