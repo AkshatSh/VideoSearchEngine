413965f54cee1aafde998f73b2b398301e5c2fd3
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2018-05-19 00:09:51 -0700

    updated config for necessary video database options

--- VideoSearchEngine/config.py
+++ VideoSearchEngine/config.py
@@ -1,1 +1,18 @@
 # all configuration options to run the video search engine
+from database_settings import (
+    VIDEO_DATABASE_USER,
+    VIDEO_DATABASE_PASSWORD,
+)
+
+
+# ---- VIDEO DATABASE CONFIGURATION SECTION ---
+VIDEO_DATABASE_NAME = "videosearchdatabase"
+VIDEO_COLLECTION_NAME = "videos"
+VIDEO_DATABASE_LOCATION = "ds229450.mlab.com:29450"
+
+VIDEO_DATABASE_URL = "mongodb://{user}:{password}@{location}/{database}".format(
+    user=VIDEO_DATABASE_USER,
+    password=VIDEO_DATABASE_PASSWORD,
+    location=VIDEO_DATABASE_LOCATION,
+    database=VIDEO_DATABASE_NAME,
+)
