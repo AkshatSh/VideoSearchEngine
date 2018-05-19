d7e2f5c0014d1c2edb5039e354360cc51d46e676
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2018-05-18 22:29:42 -0700

    Initial Noisy Frame Filter files

---
+++ VideoSearchEngine/NoisyFrameFilter.py
@@ -0,0 +1,15 @@
+# Main file for given a frame, filter the noisy frames
+
+# Main API is here, more files may be used for the implementation
+
+def get_frame_filter():
+    '''
+    return the version specified in the configuration to use
+    e.g. if there is a basic one and a complex one, the configuration should be able
+    to decide which one to use
+    '''
+    return None
+
+'''
+Describe API supported here
+'''
