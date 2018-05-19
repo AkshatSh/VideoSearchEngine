5afc993e31cd582c45883df248db84e2033ef965
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2018-05-18 22:09:44 -0700

    added initial video utils

---
+++ VideoSearchEngine/video_utils.py
@@ -0,0 +1,11 @@
+import os
+'''
+This util file is for anything related to video processing that can be factored out into here
+'''
+
+def get_frames_from_video(video_path):
+    '''
+    Given a video path, read the video, store all the frames in the array
+    and return it
+    '''
+    return []
