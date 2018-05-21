624e35198bcace88fd0a8625c301944627a3bece
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2018-05-20 23:59:37 -0700

    updated main API for object detection

--- VideoSearchEngine/ObjectDetector.py
+++ VideoSearchEngine/ObjectDetector.py
@@ -1,3 +1,9 @@
+from config import OBJECT_DETECTION_TYPE
+from ObjectDetection import (
+    TinyYolo,
+    Yolo,
+)
+
 # Main file for the bounding box object detection problem
 
 # Main API is here, more files may be used for the implementation
@@ -8,8 +14,19 @@ def get_object_detector():
     e.g. if there is a basic one and a complex one, the configuration should be able
     to decide which one to use
     '''
-    return None
+    if OBJECT_DETECTION_TYPE == "TINY_YOLO":
+        return TinyYolo.TinyYoloNet()
+    elif OBJECT_DETECTION_TYPE == "YOLO":
+        return Yolo.YoloNet()
+    else:
+        raise Exception("Unknown OBJECT_DETECTION_TYPE = {}".format(OBJECT_DETECTION_TYPE))
 
 '''
 Describe API supported here
+
+get_object_detector returns an nn.Module
+
+The main operation to use is forward on an image
+
+Take a look at ObjectDetection/detector.py for common use cases
 '''
