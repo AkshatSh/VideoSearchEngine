9f30403e466c19a3f028d5f1cd108c9e7e055b9a
Kevin Bi <kevin_bi@outlook.com>
2018-05-20 18:50:47 -0700

    Start on video_util.py; functionality to break video into frames added, updated requirments.txt

--- VideoSearchEngine/video_utils.py
+++ VideoSearchEngine/video_utils.py
@@ -1,11 +1,69 @@
+import cv2
+import numpy as np
 import os
+import argparse
+from tqdm import tqdm
 '''
 This util file is for anything related to video processing that can be factored out into here
+
+Example Usage:
+    python video_utils.py --video_path ../clips/bunny_clip.mp4 --output_path ../frames/bunny_clip/
 '''
 
 def get_frames_from_video(video_path):
     '''
     Given a video path, read the video, store all the frames in the array
-    and return it
+    and return it.
     '''
-    return []
+    # Playing video from file:
+    frameArray = []
+    cap = cv2.VideoCapture(video_path)
+
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        # Break if no image is returned (have reached end of video)
+        if frame is None or ret is False:
+            break
+
+        # Add frame to array
+        frameArray.append(frame)
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
+    return frameArray
+
+def export_video_frames(frames, output_path):
+    '''
+    Given an array of frames extracted from a video, write these frames to an output directory.
+    '''
+    if output_path != None:
+        try:
+            if not os.path.exists(output_path):
+                os.makedirs(output_path)
+        except OSError:
+            print ('Error: Creating directory of' + output_path)
+    
+    print ('Writing frames to ' + output_path)
+    currentFrame = 0
+    for frame in tqdm(frames):
+        # Saves image of the current frame in jpg file
+        name = output_path + 'frame' + str(currentFrame) + '.jpg'
+        # Write frame to directory 
+        cv2.imwrite(name, frame)
+        # To stop duplicate images
+        currentFrame += 1
+    print ("Done!")
+
+#TODO: Add arguments to: only extract every nth frame, change width/height of captured frames, etc.
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()    
+    parser.add_argument("--video_path", help="path of the video",type=str, required=True)
+    parser.add_argument("--output_path", help="output path of the video frames", type=str)
+    args = parser.parse_args()        
+    res = get_frames_from_video(args.video_path)
+    # Uncomment if frames should be written to directory.
+    #export_video_frames(res, args.output_path)
