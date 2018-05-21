0ce59cc02236201e35fa7d7b831009185ff3db37
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2018-05-20 21:17:10 -0700

    added more downloadable files

--- downloader.py
+++ downloader.py
@@ -16,6 +16,11 @@ def download_yolo_files():
     download_file_if_not_exists("https://pjreddie.com/media/files/yolov3-tiny.weights", "data/yolov3-tiny.weights")
     download_file_if_not_exists("https://pjreddie.com/media/files/yolov2-tiny.weights", "data/yolov2-tiny.weights")
     download_file_if_not_exists("https://pjreddie.com/media/files/yolo-tiny.weights", "data/yolo-tiny.weights")
+    download_file_if_not_exists("https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3.cfg", "cfg/yolov3.cfg")
+
+    download_file_if_not_exists("https://github.com/ayooshkathuria/pytorch-yolo-v3/raw/master/dog-cycle-car.png", "data/pics/dog-cycle-car.png")
+
+    download_file_if_not_exists("http://pjreddie.com/media/files/yolov3.weights", "data/yolov3.weights")
 
 def main():
     download_yolo_files()
