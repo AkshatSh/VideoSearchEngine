ca4e09447a4e747c2c068189b74d15ade8f21d0f
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2018-05-20 23:43:57 -0700

    updated downloader for necessary files

--- downloader.py
+++ downloader.py
@@ -26,6 +26,13 @@ def download_yolo_files():
 
     download_file_if_not_exists("https://github.com/ayooshkathuria/YOLO_v3_tutorial_from_scratch/raw/master/pallete", "data/pallete")
 
+    download_file_if_not_exists("https://raw.githubusercontent.com/marvis/pytorch-yolo2/master/cfg/tiny-yolo-voc.cfg", "cfg/tiny-yolo-voc.cfg")
+
+    download_file_if_not_exists("https://github.com/leetenki/YOLOtiny_v2_chainer/raw/master/tiny-yolo-voc.weights", "data/tiny-yolo-voc.weights")
+
+    download_file_if_not_exists("https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov1-tiny.cfg", "cfg/yolo-tiny.cfg")
+    download_file_if_not_exists("http://pjreddie.com/media/files/yolo-tiny.weights", "data/yolo-tiny.weights")
+
 def main():
     download_yolo_files()
 
