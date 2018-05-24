919e8789f267ad00382e988dff8441246efb23de
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2018-05-23 18:50:02 -0700

    added image captioning utilities

---
+++ VideoSearchEngine/ImageCaptioning/image_caption_utils.py
@@ -0,0 +1,7 @@
+import torch
+from torch.autograd import Variable
+
+def to_var(x, volatile=False):
+    if torch.cuda.is_available():
+        x = x.cuda()
+    return Variable(x, volatile=volatile)
