ae0c6388f5f470df1a2f121ee8f4075ede28fd4d
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2018-05-30 02:26:12 -0700

    added test data loaders

--- VideoSearchEngine/ImageCaptioningYolo/train.py
+++ VideoSearchEngine/ImageCaptioningYolo/train.py
@@ -65,22 +65,22 @@ def main(args, bbox_model):
     with open(args.vocab_path, 'rb') as f:
         vocab = pickle.load(f)
     
-    # with open(args.test_vocab_path, 'rb') as f:
-    #     test_vocab = pickle.load(f)
+    with open(args.test_vocab_path, 'rb') as f:
+        test_vocab = pickle.load(f)
     
     # Build data loader
     data_loader = get_loader(args.image_dir, args.caption_path, vocab, 
                              transform, args.batch_size,
                              shuffle=True, num_workers=args.num_workers) 
     
-    # test_data_loader = get_loader(
-    #     args.test_image_dir, 
-    #     args.test_caption_path, 
-    #     test_vocab, 
-    #     transform, 
-    #     args.batch_size, 
-    #     shuffle=True, 
-    #     num_workers=args.num_workers)
+    test_data_loader = get_loader(
+        args.test_image_dir, 
+        args.test_caption_path, 
+        test_vocab, 
+        transform, 
+        args.batch_size, 
+        shuffle=True, 
+        num_workers=args.num_workers)
 
     # Build the models
     encoder = EncoderCNN(args.embed_size).to(device)
