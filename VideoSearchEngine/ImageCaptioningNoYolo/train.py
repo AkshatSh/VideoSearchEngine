d1163283b9ed586255562acf9bf9bb9e98f913fd
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2018-05-30 20:55:29 -0700

    removed constraints on which CUDA to use

--- VideoSearchEngine/ImageCaptioningNoYolo/train.py
+++ VideoSearchEngine/ImageCaptioningNoYolo/train.py
@@ -19,9 +19,6 @@ from TensorLogger import (
     Logger
 )
 
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
-
-
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
