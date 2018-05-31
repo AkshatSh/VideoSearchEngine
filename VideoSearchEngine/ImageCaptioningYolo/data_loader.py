16b7f312a152997f336d23baac8ccc24868a53c3
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2018-05-30 17:21:44 -0700

    fixing some stuff

--- VideoSearchEngine/ImageCaptioningYolo/data_loader.py
+++ VideoSearchEngine/ImageCaptioningYolo/data_loader.py
@@ -38,8 +38,8 @@ class CocoDataset(data.Dataset):
         path = coco.loadImgs(img_id)[0]['file_name']
 
         image = Image.open(os.path.join(self.root, path)).convert('RGB')
-        if self.transform is not None:
-            image = self.transform(image)
+        #if self.transform is not None:
+        #    image = self.transform(image)
         
 
         image = np.array(image) 
@@ -51,7 +51,7 @@ class CocoDataset(data.Dataset):
         caption.extend([vocab(token) for token in tokens])
         caption.append(vocab('<end>'))
         target = torch.Tensor(caption)
-        return image, target
+        return torch.Tensor(image), target
 
     def __len__(self):
         return len(self.ids)
@@ -79,6 +79,7 @@ def collate_fn(data):
 
     # Merge images (from tuple of 3D tensor to 4D tensor).
     images = torch.stack(images, 0)
+    print(images.shape)
 
     # Merge captions (from tuple of 1D tensor to 2D tensor).
     lengths = [len(cap) for cap in captions]
@@ -157,38 +158,6 @@ class CocoYoloDataset(data.Dataset):
 
     def __len__(self):
         return len(self.ids)
-
-
-def collate_fn(data):
-    """Creates mini-batch tensors from the list of tuples (image, caption).
-    
-    We should build custom collate_fn rather than using default collate_fn, 
-    because merging caption (including padding) is not supported in default.
-
-    Args:
-        data: list of tuple (image, caption). 
-            - image: torch tensor of shape (3, 256, 256).
-            - caption: torch tensor of shape (?); variable length.
-
-    Returns:
-        images: torch tensor of shape (batch_size, 3, 256, 256).
-        targets: torch tensor of shape (batch_size, padded_length).
-        lengths: list; valid length for each padded caption.
-    """
-    # Sort a data list by caption length (descending order).
-    data.sort(key=lambda x: len(x[1]), reverse=True)
-    images, captions = zip(*data)
-
-    # Merge images (from tuple of 3D tensor to 4D tensor).
-    images = torch.stack(images, 0)
-
-    # Merge captions (from tuple of 1D tensor to 2D tensor).
-    lengths = [len(cap) for cap in captions]
-    targets = torch.zeros(len(captions), max(lengths)).long()
-    for i, cap in enumerate(captions):
-        end = lengths[i]
-        targets[i, :end] = cap[:end]        
-    return images, targets, lengths
 
 def get_loader_yolo(root, json, vocab, bbox_model, transform, batch_size, shuffle, num_workers):
     """Returns torch.utils.data.DataLoader for custom coco dataset."""
