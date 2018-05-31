ab5f6620eddac33c8ac7c6a310172b1ab2cfcba2
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2018-05-30 20:28:50 -0700

    updated batch size

--- VideoSearchEngine/ImageCaptioningYolo/im_args.py
+++ VideoSearchEngine/ImageCaptioningYolo/im_args.py
@@ -21,7 +21,7 @@ def get_arg_parse():
                         help='layout encoding size')
     
     parser.add_argument('--num_epochs', type=int, default=5)
-    parser.add_argument('--batch_size', type=int, default=4)
+    parser.add_argument('--batch_size', type=int, default=128)
     parser.add_argument('--num_workers', type=int, default=2)
     parser.add_argument('--learning_rate', type=float, default=0.001)
     args = parser.parse_args()
