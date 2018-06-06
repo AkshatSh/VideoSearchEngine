abd3f34f3338a32622c158a3b9547b6b893be51a
Sarah Yu <sarahyu@cs.washington.edu>
2018-06-05 23:14:42 -0700

    updated

--- VideoSearchEngine/database_settings.py
+++ VideoSearchEngine/database_settings.py
@@ -1,5 +1,5 @@
 # Update for the User name and Password for the
 # database service
 
-VIDEO_DATABASE_USER = "<USER_NAME>"
-VIDEO_DATABASE_PASSWORD = "<PASSWORD>"
+VIDEO_DATABASE_USER = "video_search"
+VIDEO_DATABASE_PASSWORD = "passw0rd"
